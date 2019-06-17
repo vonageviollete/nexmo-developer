30666ba1d14fc5bf8d6574c77ff06284c5d2de30
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-06-17 16:34:45 +0200

    Add basic smartling integration.

---
+++ app/services/smartling_api.rb
@@ -0,0 +1,38 @@
+class SmartlingAPI
+  def initialize(user_id:, user_secret:, project_id:)
+    @client = Smartling::File.new(
+      userId: user_id,
+      userSecret: user_secret,
+      projectId: project_id
+    )
+  end
+
+  def upload(filename)
+    file_uri = file_uri(filename)
+    wrap_in_rescue { @client.upload(filename, file_uri, 'markdown', 'smartling.placeholder_format_custom': '§§.+?§§') }
+  end
+
+  def last_modified(filename:, locale:)
+    file_uri = file_uri(filename)
+    wrap_in_rescue { @client.last_modified(file_uri, locale) }
+  end
+
+  def download_translated(filename:, locale:, type: :published)
+    file_uri = file_uri(filename)
+    wrap_in_rescue { @client.download_translated(file_uri, locale, retrievalType: type) }
+  end
+
+  private
+
+  def file_uri(filename)
+    filename.gsub(/_documentation\/[a-z]{2}\//, '')
+  end
+
+  def wrap_in_rescue
+    begin
+      yield
+    rescue => e
+    end
+  end
+
+end
