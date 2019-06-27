3e5e7a838f4c507831aa67bf66d2dd2077e36293
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-06-27 06:21:24 +0200

    Remove unused methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,10 +22,6 @@ module ApplicationHelper
     path.gsub(/.*#{@namespace_root}/, '').gsub('.md', '')
   end
 
-  def url_to_configuration_identifier(url)
-    url.tr('/', '.').sub(/^./, '')
-  end
-
   def show_canonical_meta?
     return true if params[:code_language].present?
     return true if Rails.env.production? && request.base_url != 'https://developer.nexmo.com'
@@ -39,27 +35,6 @@ module ApplicationHelper
   def canonical_url
     base_url = Rails.env.production? ? 'https://developer.nexmo.com' : request.base_url
     canonical_path.prepend(base_url)
-  end
-
-  def normalize_summary_title(summary, operation_id)
-    # return summary early if provided
-    return summary unless summary.nil?
-
-    # If the operation ID is camelCase,
-    if operation_id.match?(/^[a-zA-Z]\w+(?:[A-Z]\w+){1,}/x)
-      # Use the rails `.underscore` method to convert someString to some_string
-      operation_id = operation_id.underscore
-    end
-
-    # Replace snake_case and kebab-case with spaces and titelize the string
-    operation_id = operation_id.gsub(/(_|-)/, ' ').titleize
-
-    # Some terms need to be capitalised all the time
-    uppercase_array = ['SMS', 'DTMF']
-    operation_id.split(' ').map do |c|
-      next c.upcase if uppercase_array.include?(c.upcase)
-      c
-    end.join(' ')
   end
 
   def dashboard_cookie(campaign)
