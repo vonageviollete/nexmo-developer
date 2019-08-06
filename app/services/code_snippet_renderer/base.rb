7e9a82d4583eb05ef63f682ff2f254c202dc80ee
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-08-06 14:38:15 +0200

    Make rubocop happy.

--- app/services/code_snippet_renderer/base.rb
+++ app/services/code_snippet_renderer/base.rb
@@ -2,7 +2,7 @@ module CodeSnippetRenderer
   class Base
     def self.inherited(base)
       base.extend ActionView::Helpers::TranslationHelper
-      base.instance_variable_set :@virtual_path, "services.#{base.name.underscore.gsub('/', '.')}"
+      base.instance_variable_set :@virtual_path, "services.#{base.name.underscore.tr('/', '.')}"
     end
   end
 end
